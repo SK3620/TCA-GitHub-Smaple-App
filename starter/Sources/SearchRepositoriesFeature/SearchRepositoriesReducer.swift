62e70235994b6c9cbf0eea4c976120cbf9a1e23f
SK3620 <k.n.t11193620@gmail.com>
2025-03-28 11:44:15 +0900

    add: Githubリポジトリ検索のState, Action, Reducerを追加

---
+++ starter/Sources/SearchRepositoriesFeature/SearchRepositoriesReducer.swift
@@ -0,0 +1,43 @@
+import ComposableArchitecture
+import SwiftUI
+import Foundation
+
+@Reducer
+public struct SearchRepositoriesReducer: Reducer {
+    // MARK: - State
+    public struct State {
+        var items: [RepositoryItem] = []
+        @BindingState var query: String = ""
+        @BindingState var showFavoritesOnly = false
+        var hasMorePage = false
+        
+        var filteredItems: [RepositoryItem] {
+            return self.items.filter { !showFavoritesOnly || $0.liked }
+        }
+        
+        public init() {}
+    }
+    
+    public enum Action: BindableAction {
+        case binding(BindingAction<State>) // バインディング変更時のアクション
+        case itemAppeared(id: Int) // リストのアイテムが表示されたときのアクション
+    }
+    
+    public var body: some ReducerOf<Self> {
+        BindingReducer() // BindingActionを受け取った時の、"@BindingState"を更新するためのReducer
+        Reduce { state, action in
+            switch action {
+            case .binding:
+                return .none // BindingReducer()で自動で処理されるので特に何もしなくてOK
+            case .itemAppeared(let id):
+                return .none // アイテムが表示されたときの処理（現在は未実装）
+            }
+        }
+    }
+}
+
+public struct RepositoryItem: Identifiable {
+    public let id: Int
+    public let name: String
+    public let liked: Bool
+}
