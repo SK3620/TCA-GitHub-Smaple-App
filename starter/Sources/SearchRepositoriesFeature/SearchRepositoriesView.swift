68f579ebe0cf4687b22f7f905322f30ba1e8bfe9
SK3620 <k.n.t11193620@gmail.com>
2025-03-28 11:45:26 +0900

    update: サンプルとしてGitHubレポジトリの一覧を表示する画面を追加

---
+++ starter/Sources/SearchRepositoriesFeature/SearchRepositoriesView.swift
@@ -0,0 +1,56 @@
+import SwiftUI
+import ComposableArchitecture
+import RepositoryDetailFeature
+
+// 検索リポジトリ画面を表示する View
+public struct SearchRepositoriesView: View {
+    // 状態とアクションを管理するStore
+    let store: StoreOf<SearchRepositoriesReducer>
+    
+    // ViewState は UI に必要なデータをまとめた構造体
+    struct ViewState: Equatable {
+        @BindingViewState var query: String // 検索クエリのバインディング
+        @BindingViewState var showFavoritesOnly: Bool // お気に入りのみを表示するかのバインディング
+        let hasMorePage: Bool // さらにページがあるかどうか
+        
+        @MainActor
+        init(store: BindingViewStore<SearchRepositoriesReducer.State>) {
+            self._query = store.$query
+            self._showFavoritesOnly = store.$showFavoritesOnly
+            self.hasMorePage = store.hasMorePage
+        }
+    }
+    
+    public init(store: StoreOf<SearchRepositoriesReducer>) {
+        self.store = store
+    }
+    
+    public var body: some View {
+        // ViewStore を使用して、状態を監視しながら UI を構築
+        WithViewStore(store, observe: ViewState.init(store:)) { viewStore in
+            List {
+                // お気に入りのリポジトリのみを表示するためのトグルスイッチ
+                Toggle(isOn: viewStore.$showFavoritesOnly) {
+                    Text("Favorites Only")
+                }
+                
+                // フィルタされたリポジトリをリスト表示
+//                ForEach(viewStore.filteredItems) { item in
+//                    Text(item.name)
+//                        .onAppear {
+//                            // アイテムが表示されたときにイベントを送信
+//                            viewStore.send(.itemAppeared(id: item.id))
+//                        }
+//                }
+                
+                // さらにデータがある場合はローディングインジケータを表示
+                if viewStore.hasMorePage {
+                    ProgressView()
+                        .frame(maxWidth: .infinity)
+                }
+            }
+            // 検索機能を追加
+            .searchable(text: viewStore.$query)
+        }
+    }
+}
