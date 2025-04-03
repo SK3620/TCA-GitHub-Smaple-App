85d9ef71515474a8e175920533227cc9dc7be578
SK3620 <k.n.t11193620@gmail.com>
2025-04-03 23:36:30 +0900

    update: WithViewStoreは非推奨 @ObservableStateを推奨

--- starter/LearningMemo/WithViewStore.swift
+++ starter/LearningMemo/WithViewStore.swift
@@ -7,7 +7,14 @@
 
 // MARK: - WithViewStore
 /*
+ Deprecated
+
+ Use '@ObservableState', instead. See the following migration guide for more information: https://pointfreeco.github.io/swift-composable-architecture/main/documentation/composablearchitecture/migratingto1.7#Using-ObservableState
+ 
  Store のデータ (State) を SwiftUI で簡単に扱えるようにするラッパー
+ Viewによってstoreが持つReducerで管理しているState（状態）の変化を監視できるようにする
+ A view helper that transforms a Store into a ViewStore so that its state can be observed by a view builder.
+
  
  let store = 〜〜〜
  WithViewStore(store) { viewStore in
@@ -17,4 +24,3 @@
  ・WithViewStore を使うと、その store から State を リアクティブに 取り出せる
  ・さらに、viewStore.$query のように $ をつけると 双方向バインディング（Binding） できる！
 */
-
