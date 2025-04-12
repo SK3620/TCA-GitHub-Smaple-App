b3825eeaef88cff6960f8b9e2f41ce3d93abb0ed
SK3620 <k.n.t11193620@gmail.com>
2025-04-12 13:32:18 +0900

    update: オブジェクトを依存関係として定義

---
+++ starter/Sources/GithubClientLive/GithubClient+Live.swift
@@ -0,0 +1,30 @@
+import GithubClient
+import Dependencies
+import SharedModel
+import ApiClient
+
+extension GithubClient: DependencyKey {
+    public static let liveValue: GithubClient = .live()
+
+    static func live(apiClient: ApiClient = .liveValue) -> Self {
+        .init(
+            searchRepos: { query, page in
+                try await apiClient.send(request: SearchReposRequest(query: query, page: page))
+            }
+        )
+    }
+}
+
+/*
+extension GithubClient: DependencyKey {
+    public static let liveValue: GithubClient = {
+        let apiClient = ApiClient.liveValue
+
+        return .init(
+            searchRepos: { query, page in
+                try await apiClient.send(request: SearchReposRequest(query: query, page: page))
+            }
+        )
+    }()
+}
+*/
