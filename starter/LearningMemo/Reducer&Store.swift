6c0d3417a1efd674df5d8d907f90526036971c1e
SK3620 <k.n.t11193620@gmail.com>
2025-05-04 15:22:17 +0900

    add: Reducer＆Store学習メモを追加

---
+++ starter/LearningMemo/Reducer&Store.swift
@@ -0,0 +1,17 @@
+//
+//  Reducer.swift
+//  GithubApp
+//
+//  Created by 鈴木 健太 on 2025/05/04.
+//
+
+// MARK: - Reducer
+// Reducerとは「今の状態 (State)」と「実行された操作 (Action)」を受け取って、状態を変更する処理を書く場所」 のこと
+
+// Reducerとは、「ユーザー操作などによって実行されたアクション（Action）」を受け取って、それに応じて、「現在の状態（State）」を更新、変更する処理、指示を出すやつ
+
+// MARK: - Store
+
+// 状態 (State) とアクション (Action) の管理 を担うやつ
+// typealias StoreOf<R> = Store<R.State, R.Action> where R : Reducer
+
